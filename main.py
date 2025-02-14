5b5f4a6e9d65d558df487fa29be3c041b1182112
aelaraby6 <body1262005@gmail.com>
2025-02-14 15:31:16 +0200

    add new changes

--- main.py
+++ main.py
@@ -22,55 +22,9 @@ def main():
             libraryManager.searchBook(library)
         
         elif choice == "4":
-            # Update book logic here
-             title_or_id = input("Would you like to update (1) Book ID or (2) Title? Enter 1 or 2: ")
-
-                if title_or_id == "1":
-                    current_id = input("Enter the current Book ID you want to update: ")
-                    found = False
-                    for book in library:
-                        if book.get_book_id() == current_id:
-                            found = True
-                            new_id = input("Enter the new Book ID to replace the old one: ")
-                            book.set_book_id(new_id)
-                            print(f"Book ID updated successfully!")
-                            break
-                    if not found:
-                        print("Book not found.")
-
-                elif title_or_id == "2":
-                    current_title = input("Enter the current Book Title you want to update: ")
-                    found = False
-                    for book in library:
-                        if book.get_title().lower() == current_title.lower():
-                            found = True
-                            new_title = input("Enter the new Book Title to replace the old one: ")
-                            book.set_title(new_title)
-                            print(f"Book Title updated successfully!")
-                            break
-                    if not found:
-                        print("Book not found.")
-
-                else:
-                    print("Invalid choice! Please enter 1 or 2.")
-            pass
+            libraryManager.updateBook(library)
         elif choice == "5":
-            # Delete book logic here
-                elif choice == "5":
-            if not library:
-                print("There are no books to delete")
-            else:
-                delete_id = input("Enter the book id: ")
-                found = False
-                for book in library:
-                    if book.book_id == delete_id:
-                        library.remove(book)
-                        print("The book has been deleted")
-                        found = True
-                        break
-                if not found:
-                    print("The book was not found")
-            pass
+            libraryManager.deleteBook(library)
         elif choice == "6":
             database.saveBookstoDB(library)
         
